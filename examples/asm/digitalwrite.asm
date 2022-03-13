5f1c560ea269b9321721b8cd6f0953c1a634c510
nickgammon <nick@gammon.com.au>
2022-03-13 15:48:14 +1100

    Fixed to match more recent approach to symbols

--- examples/asm/digitalwrite.asm
+++ examples/asm/digitalwrite.asm
@@ -1,18 +1,7 @@
-
-
 ;
 ;  digitalWrite example
 ;
   jmp start     ; skip the variable declarations
-
-;
-;  VIA functions
-;
-pinmode_           = $809f     ; sets the pin mode - pin:0 to 15; mode: 0 or 1 : pin in A, mode in X
-digitalread_       = $80a2     ; reads a pin - pin:0 to 15; Pin to read is in in A. Returns zero or non-zero in A.
-digitalwrite_      = $80a5     ; writes to a pin - pin:0 to 15; value: 0 or 1 : pin in A, mode in X
-
-delay_             = $80ae     ; delay for YYXX ms (Y = high-order byte, X = lo-order byte)
 
 counter   dfb 0     ; how many toggles we did
 pin_state dfb 0     ; current pin state
@@ -29,7 +18,7 @@ start:
 ;
   lda #2   ; Port PA2
   ldx #1   ; write mode
-  jsr pinmode_
+  jsr pinmode
 
 
 write_loop:
@@ -41,13 +30,13 @@ write_loop:
   sta pin_state
   tax
   lda #2   ; Port PA2
-  jsr digitalwrite_
+  jsr digitalwrite
 ;
 ;  delay 500 ms
 ;
   ldx #<500
   ldy #>500
-  jsr delay_
+  jsr delay
 ;
 ;  do it ITERATIONS times
 ;
