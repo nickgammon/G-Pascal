bb1c35a02321b392df81e3d53c4582b82b374c61
nickgammon <nick@gammon.com.au>
2022-03-13 15:49:57 +1100

    Improved code

--- examples/asm/hello_world_relocated.asm
+++ examples/asm/hello_world_relocated.asm
@@ -1,12 +1,11 @@
   SYM $5800     ; symbols are to be placed at $5800 (growing downwards)
   ORG $5800     ; code is to be placed at $5800 (growing upwards)
 
-      jmp start   ; skip the message
-hello ascii "Hello, world!"
-      dfb   $0A
-      dfb   0
-start = *
-      lda #<hello
-      ldx #>hello
-      jsr serial_print_message
-      rts
+  jmp start   ; skip the message
+hello asciiz "Hello, world!\n"
+
+start:
+  lda #<hello
+  ldx #>hello
+  jsr serial_print_message
+  rts
